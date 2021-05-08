059560c429e10d0bac6eec71bd00796343c6f980
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-08 21:53:14 +0430

    setup Rating

---
+++ src/main/java/ir/ac/kntu/models/Rating.java
@@ -0,0 +1,17 @@
+package ir.ac.kntu.models;
+
+public enum Rating {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5);
+    private final int value;
+    Rating(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
