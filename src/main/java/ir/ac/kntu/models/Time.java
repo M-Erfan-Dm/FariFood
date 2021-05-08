6079827117580729b5c76e592ed26a4bc750dd44
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-08 23:08:54 +0430

    setup Time

---
+++ src/main/java/ir/ac/kntu/models/Time.java
@@ -0,0 +1,47 @@
+package ir.ac.kntu.models;
+
+import java.util.Objects;
+
+public class Time {
+    private int hour;
+
+    private int minute;
+
+    public Time(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        Time time = (Time) o;
+        return hour == time.hour && minute == time.minute;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hour, minute);
+    }
+}
