4299f1091fd633b284f2487f66facf3c11f77761
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-09 23:46:55 +0430

    setup CourierWorkInfo

---
+++ src/main/java/ir/ac/kntu/models/CourierWorkInfo.java
@@ -0,0 +1,58 @@
+package ir.ac.kntu.models;
+
+import java.util.Objects;
+
+public class CourierWorkInfo {
+    private Restaurant restaurant;
+
+    private Schedule schedule;
+
+    private Salary salary;
+
+    public CourierWorkInfo(Restaurant restaurant, Schedule schedule, Salary salary) {
+        this.restaurant = restaurant;
+        this.schedule = schedule;
+        this.salary = salary;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public Salary getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Salary salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        CourierWorkInfo that = (CourierWorkInfo) o;
+        return restaurant.equals(that.restaurant) && schedule.equals(that.schedule) && salary.equals(that.salary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(restaurant, schedule, salary);
+    }
+}
