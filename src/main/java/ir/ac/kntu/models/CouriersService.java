f2cd5209d682db4c6c8b9900353973c71542f574
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-10 03:18:27 +0430

    add methods to CouriersService

--- src/main/java/ir/ac/kntu/models/CouriersService.java
+++ src/main/java/ir/ac/kntu/models/CouriersService.java
@@ -1,13 +1,18 @@
 package ir.ac.kntu.models;
 
+import ir.ac.kntu.db.CouriersDB;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 public class CouriersService {
+    private final CouriersDB couriersDB;
+
     private Set<Courier> couriers;
 
-    public CouriersService(Set<Courier> couriers) {
+    public CouriersService(CouriersDB couriersDB, Set<Courier> couriers) {
+        this.couriersDB = couriersDB;
         this.couriers = couriers;
     }
 
@@ -19,6 +24,42 @@ public class CouriersService {
         this.couriers = couriers;
     }
 
+    public boolean hireCourier(String courierPhoneNumber){
+        Courier courier = couriersDB.getCourierByPhoneNumber(courierPhoneNumber);
+        if (courier==null){
+            return false;
+        }
+        couriers.add(courier);
+        return true;
+    }
+
+    public boolean dismissCourier(String courierPhoneNumber){
+        for (Courier courier : couriers){
+            if (courier.getPhoneNumber().equals(courierPhoneNumber)){
+                couriers.remove(courier);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasHiredCourier(String phoneNumber){
+        for (Courier courier : couriers){
+            if (courier.getPhoneNumber().equals(phoneNumber)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Courier getCourierByPhoneNumber(String phoneNumber){
+        for (Courier courier : couriers){
+            if (courier.getPhoneNumber().equals(phoneNumber)){
+                return courier;
+            }
+        }
+        return null;
+    }
 
     @Override
     public boolean equals(Object o) {
