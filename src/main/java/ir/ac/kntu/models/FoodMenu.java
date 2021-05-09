a4044328eb8db3862e87de8c596750656f97d2ca
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-10 02:43:38 +0430

    setup FoodMenu

---
+++ src/main/java/ir/ac/kntu/models/FoodMenu.java
@@ -0,0 +1,51 @@
+package ir.ac.kntu.models;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class FoodMenu {
+    private Set<Food> foods;
+
+    public FoodMenu(Set<Food> foods) {
+        this.foods = foods;
+    }
+
+    public Set<Food> getFoods() {
+        return new HashSet<>(foods);
+    }
+
+    public void setFoods(Set<Food> foods) {
+        this.foods = foods;
+    }
+
+    public void addFood(Food food){
+        foods.add(food);
+    }
+
+    public void removeFood(Food food){
+        foods.remove(food);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        FoodMenu foodMenu = (FoodMenu) o;
+        return foods.equals(foodMenu.foods);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(foods);
+    }
+
+    @Override
+    public String toString() {
+        return "foods=" + foods;
+    }
+}
