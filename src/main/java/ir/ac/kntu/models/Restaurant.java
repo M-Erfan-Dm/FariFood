d395dfcd45e1f642d8d14adc6bf6971b7808d2cf
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-10 00:55:47 +0430

    setup Restaurant

---
+++ src/main/java/ir/ac/kntu/models/Restaurant.java
@@ -0,0 +1,140 @@
+package ir.ac.kntu.models;
+
+import java.util.Objects;
+
+public class Restaurant {
+
+    private int id;
+
+    private String name;
+
+    private String address;
+
+    private FoodMenu foodMenu;
+
+    private Schedule schedule;
+
+    private RestaurantPriceType priceType;
+
+    private double rating = 5;
+
+    private CouriersService couriers;
+
+    private OrdersService orders;
+
+    public Restaurant(int id, String name, String address, FoodMenu foodMenu,
+                      Schedule schedule, RestaurantPriceType priceType
+            , CouriersService couriers, OrdersService orders) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.foodMenu = foodMenu;
+        this.schedule = schedule;
+        this.priceType = priceType;
+        this.couriers = couriers;
+        this.orders = orders;
+    }
+
+    public Restaurant(String name, String address, FoodMenu foodMenu,
+                      Schedule schedule, RestaurantPriceType priceType
+            , CouriersService couriers, OrdersService orders) {
+        this.name = name;
+        this.address = address;
+        this.foodMenu = foodMenu;
+        this.schedule = schedule;
+        this.priceType = priceType;
+        this.couriers = couriers;
+        this.orders = orders;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public FoodMenu getFoodMenu() {
+        return foodMenu;
+    }
+
+    public void setFoodMenu(FoodMenu foodMenu) {
+        this.foodMenu = foodMenu;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public RestaurantPriceType getPriceType() {
+        return priceType;
+    }
+
+    public void setPriceType(RestaurantPriceType priceType) {
+        this.priceType = priceType;
+    }
+
+    public CouriersService getCouriers() {
+        return couriers;
+    }
+
+    public void setCouriers(CouriersService couriers) {
+        this.couriers = couriers;
+    }
+
+    public OrdersService getOrders() {
+        return orders;
+    }
+
+    public void setOrders(OrdersService orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        Restaurant that = (Restaurant) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", schedule=" + schedule +
+                ", priceType=" + priceType +
+                ", rating=" + rating ;
+    }
+}
