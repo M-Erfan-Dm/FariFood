d20ee620c8a5b47c3313c26509849c8137ebd654
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-12 01:34:00 +0430

    complete getFeedbacksOfFood method in OrdersService

--- src/main/java/ir/ac/kntu/models/OrdersService.java
+++ src/main/java/ir/ac/kntu/models/OrdersService.java
@@ -2,11 +2,7 @@ package ir.ac.kntu.models;
 
 import ir.ac.kntu.utils.IdGenerator;
 
-import java.awt.image.AreaAveragingScaleFilter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class OrdersService {
     private Set<Order> orders;
@@ -23,15 +19,15 @@ public class OrdersService {
         this.orders = orders;
     }
 
-    public void addOrder(Order order){
-        Order newOrder = new Order(IdGenerator.generateNewId(),order.getFood(),order.getFeedback(),
-                order.getCustomer(),order.getCourier(),order.getOrderState());
+    public void addOrder(Order order) {
+        Order newOrder = new Order(IdGenerator.generateNewId(), order.getFood(), order.getFeedback(),
+                order.getCustomer(), order.getCourier(), order.getOrderState());
         orders.add(newOrder);
     }
 
-    public boolean removeOrder(int orderId){
-        for (Order order : orders){
-            if (order.getId()==orderId){
+    public boolean removeOrder(int orderId) {
+        for (Order order : orders) {
+            if (order.getId() == orderId) {
                 orders.remove(order);
                 return true;
             }
@@ -39,83 +35,93 @@ public class OrdersService {
         return false;
     }
 
-    public Order getOrderById(int orderId){
-        for (Order order : orders){
-            if (order.getId()==orderId){
+    public Order getOrderById(int orderId) {
+        for (Order order : orders) {
+            if (order.getId() == orderId) {
                 return order;
             }
         }
         return null;
     }
 
-    public Set<Order> getOrdersByCustomer(Customer customer){
+    public Set<Order> getOrdersByCustomer(Customer customer) {
         Set<Order> foundOrders = new HashSet<>();
-        for (Order order : orders){
-            if (order.getCustomer().equals(customer)){
+        for (Order order : orders) {
+            if (order.getCustomer().equals(customer)) {
                 foundOrders.add(order);
             }
         }
         return foundOrders;
     }
 
-    public Set<Order> getOrdersByCourier(Courier courier){
+    public Set<Order> getOrdersByCourier(Courier courier) {
         Set<Order> foundOrders = new HashSet<>();
-        for (Order order : orders){
-            if (order.getCourier().equals(courier)){
+        for (Order order : orders) {
+            if (order.getCourier().equals(courier)) {
                 foundOrders.add(order);
             }
         }
         return foundOrders;
     }
 
-    public Set<Order> getOrdersByFood(Food food){
+    public Set<Order> getOrdersByFood(Food food) {
         Set<Order> foundOrders = new HashSet<>();
-        for (Order order : orders){
-            if (order.getFood().equals(food)){
+        for (Order order : orders) {
+            if (order.getFood().equals(food)) {
                 foundOrders.add(order);
             }
         }
         return foundOrders;
     }
 
-    public Set<Order> getOrdersByState(OrderState orderState){
+    public Set<Order> getOrdersByState(OrderState orderState) {
         Set<Order> foundOrders = new HashSet<>();
-        for (Order order : orders){
-            if (order.getOrderState()==orderState){
+        for (Order order : orders) {
+            if (order.getOrderState() == orderState) {
                 foundOrders.add(order);
             }
         }
         return foundOrders;
     }
 
-    public ArrayList<Feedback> getAllFeedbacks(){
+    public ArrayList<Feedback> getAllFeedbacks() {
         ArrayList<Feedback> feedbacks = new ArrayList<>();
-        for (Order order : orders){
+        for (Order order : orders) {
             Feedback feedback = order.getFeedback();
-            if (feedback!=null){
+            if (feedback != null) {
                 feedbacks.add(feedback);
             }
         }
         return feedbacks;
     }
 
-    public int getCountOfAllFeedbacks(){
+    public int getCountOfAllFeedbacks() {
         int count = 0;
-        for (Order order : orders){
+        for (Order order : orders) {
             Feedback feedback = order.getFeedback();
-            if (feedback!=null){
+            if (feedback != null) {
                 count++;
             }
         }
         return count;
     }
 
+    public List<Feedback> getFeedbacksOfFood(Food food) {
+        List<Feedback> feedbacks = new ArrayList<>();
+        for (Order order : orders) {
+            if (order.getFood().equals(food) && order.getFeedback() != null) {
+                feedbacks.add(order.getFeedback());
+            }
+        }
+        return feedbacks;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o){
+        if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()){
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         OrdersService that = (OrdersService) o;
