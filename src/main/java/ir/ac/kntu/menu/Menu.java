d68c53250e52b07b520b186e1c95a151013f9028
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-16 19:50:16 +0430

    complete getSchedule and getRestaurantPriceType in Menu

--- src/main/java/ir/ac/kntu/menu/Menu.java
+++ src/main/java/ir/ac/kntu/menu/Menu.java
@@ -1,7 +1,14 @@
 package ir.ac.kntu.menu;
 
 import ir.ac.kntu.menu.settings.SettingsOption;
+import ir.ac.kntu.models.Day;
+import ir.ac.kntu.models.RestaurantPriceType;
+import ir.ac.kntu.models.Schedule;
+import ir.ac.kntu.models.Time;
 import ir.ac.kntu.utils.ScannerWrapper;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class Menu {
 
@@ -13,6 +20,7 @@ public abstract class Menu {
         if (choice >= 0 && choice < options.length) {
             return options[choice];
         }
+        System.out.println("Wrong choice!");
         return null;
     }
 
@@ -39,5 +47,72 @@ public abstract class Menu {
     public String getAddress(){
         System.out.println("Enter address : ");
         return ScannerWrapper.nextLine();
+    }
+
+    public Time getTime(){
+        System.out.println("Enter hour :");
+        int hour = Integer.parseInt(ScannerWrapper.nextLine());
+        if (hour<0 || hour>23){
+            System.out.println("Wrong hour!");
+            return null;
+        }
+        int minute = Integer.parseInt(ScannerWrapper.nextLine());
+        if (minute<0 || minute>59){
+            System.out.println("Wrong minute!");
+            return null;
+        }
+        return new Time(hour,minute);
+    }
+
+    public Set<Day> getDays(){
+        System.out.println(
+                "1.Sunday\n" +
+                "2.Monday\n" +
+                "3.Tuesday\n" +
+                "4.Wednesday\n" +
+                "5.Thursday\n" +
+                "6.Friday\n" +
+                "7.Saturday\n");
+        System.out.println("Enter the days (or 0 to stop) :");
+        Set<Day> days = new HashSet<>();
+        int day = Integer.parseInt(ScannerWrapper.nextLine())-1;
+        Day[] daysNames = Day.values();
+        while (day>=0 && day<7){
+            days.add(daysNames[day]);
+            if (days.size()==7){
+                break;
+            }
+            day = Integer.parseInt(ScannerWrapper.next())-1;
+        }
+        return days;
+    }
+
+    public Schedule getSchedule(){
+        System.out.println("Enter opening time : ");
+        Time startTime = getTime();
+        if (startTime == null){
+            return null;
+        }
+        System.out.println("Enter closing time : ");
+        Time endTime = getTime();
+        if (endTime == null){
+            return null;
+        }
+        System.out.println("Working days :");
+        Set<Day> days = getDays();
+        if (days.size()==0){
+            System.out.println("No days entered!");
+            return null;
+        }
+        return new Schedule(startTime,endTime,days);
+    }
+
+    public RestaurantPriceType getRestaurantPriceType(){
+        System.out.println(
+                "1.Economical\n" +
+                "2.Middle\n" +
+                "3.Luxurious\n");
+        System.out.println("Enter restaurant price type : ");
+        return getOption(RestaurantPriceType.class);
     }
 }
